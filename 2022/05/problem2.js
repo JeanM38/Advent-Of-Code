b0bbcd5606755b732bb8ed432b37cdfd488e5d77
Jean Mionnet <jmionnet@emagineurs.com>
2022-12-05 14:20:40 +0100

    2022/05 - part 02

--- 2022/05/problem2.js
+++ 2022/05/problem2.js
@@ -1,11 +1,53 @@
 const fs = require('fs');
 
 const fileContent = fs.readFileSync('./input.txt', 'utf-8');
-// const lines = fileContent.split(/\r?\n/); => Not always usefull
+const lines = fileContent.split(/\r?\n/);
+const linesLength = lines[0].length;
 
-fileContent.split(/\r?\n/).forEach(line =>  {
-    /* Algo comes here */
-});
+let stacks = [];
+
+for (let i = 0; i <= lines.length; ++i) {
+    let stack = [];
+
+    /* Every crate id is uppercase, by column, save it in an array */
+    for (let j = 0; j <= linesLength; ++j) {
+        if (lines[j] != undefined && /^[A-Z]+$/.test(lines[j][i]) ) {
+            stack = [...stack, lines[j][i]];
+        }
+    }
+    /* if column get 1 crate min. */
+    if (stack.length) {
+        stacks = [...stacks, stack];
+    }
+}
+
+for (let i = 0; i < lines.length; ++i) {
+    /* Get only instructions */
+    if (/^[a-z]+$/.test(lines[i][0]) && lines[i] !== '') {
+        const guidelineDirty = lines[i].split(' ');
+        
+        /* Create guideline object from instructions */
+        const guideline = { 
+            "amount": parseInt(guidelineDirty[1]),
+            "from": parseInt(guidelineDirty[3]) -1, /* -1 Align to JS Array start index of 0 */
+            "to": parseInt(guidelineDirty[5]) - 1,
+        }
+
+        /* Get stack items to remove */
+        let itemsToRemove = [...stacks[guideline.from]].splice(0, guideline.amount);
+
+        /* Add theme to target */
+        stacks[guideline.to] = [...itemsToRemove, ...stacks[guideline.to]];
+
+        /* Remove them from previous stack */
+        stacks[guideline.from].splice(0, guideline.amount);
+    }
+}
+
+/* Get head of each stack */
+const topOfEach = stacks.map(st => st[0]).join('');
+
+console.log(topOfEach);
 
 /* Calculate and log memory used during process */
 const used = process.memoryUsage().heapUsed / 1024 / 1024;
